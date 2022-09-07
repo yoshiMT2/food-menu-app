5bcab2ee0a641a5581582db19577c2f7e9432263
Yoshiyuki Matsumoto <81567061+yoshiMT2@users.noreply.github.com>
2022-09-08 03:48:58 +0900

    Configured CSS passing with nginx

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/static/'
+STATIC_URL = '/django_static/static/'
 
 STATIC_ROOT = '/vol/web/static'
 
