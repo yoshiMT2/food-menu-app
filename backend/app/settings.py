a3f82d0efeabe17d297dcf0dfa41fb27a2b84545
松本芳行 <yoshi@matsumotoyoshiyukis-MacBook-Air.local>
2022-10-23 19:14:12 +0900

    Fixed static url in settings

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -130,9 +130,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/django_static/static/'
+STATIC_URL = 'static/'
 
-STATIC_ROOT = '/vol/web/static'
+# STATIC_ROOT = '/vol/web/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
